592afac4f86d4c3d0f07f34d2c13392ea2b0b21c
Sergey <blackmoloch.cc@gmail.com>
2023-10-25 16:12:30 +0300

    fix(game/variants): fixed text block

--- src/components/variant-button/variant-button.tsx
+++ src/components/variant-button/variant-button.tsx
@@ -15,6 +15,7 @@ interface GetClasses {
   isChosen:boolean;
   id: string|number;
 }
+
 const getAdditionClass = ({id, rightAnswer, isChosen}: GetClasses) => {
   const isRightVariant = String(rightAnswer) === String(id);
   if (rightAnswer === null ) {
@@ -50,6 +51,7 @@ export function VariantButton ({variant, testId}: VariantButtonProps) {
   return (
     <li>
       <button
+        title={variant.enName || undefined}
         type="button"
         className={btnClasses}
         onClick={handleButtonClick}
--- src/utils/component-utils.ts
+++ src/utils/component-utils.ts
@@ -1,8 +1,8 @@
 import { QuestionVariant } from "../types/game-types";
-import { IVariant } from "../types/question-type";
 
 export const getVariantText = (question: QuestionVariant) => {
-  const {enName, name, slogan, year} = question;
+  const {name, slogan, year} = question;
   if (slogan) return slogan;
-  return `${name || ''}${enName ? `(${enName})`  : ''}${year ? `, ${year}`  : ''}`
+  if (!name && year) return year;
+  return `${name || ''}${year ? `, ${year}`  : ''}`
 }
